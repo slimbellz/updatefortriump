190051f87737d4ccfedfb91fd8c36af619d6f1ef
Teslim <slimbellz@gmail.com>
2020-07-27 10:42:16 +0100

    newly created

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    //toggler btn
+    $('.navbar-toggler').click(function(){
+        $('.navbar-toggler').toggleClass('change')
+    })
+
+    // sticky scroll bar
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+
+        if (position >= 718) {
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        } else {
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        }
+    })
+
+    //smooth scroll
+    $('.nav-items a, .header-link, #back-to-top').click(function(link){
+        link.preventDefault();
+
+        let target = $(this).attr('href');
+
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top - 25
+        }, 3000);
+    })
+
+
+
+
+    $("#header, .info").ripples({
+        dropRadius: 25,
+        perturbance: 0.6,
+      });
+      //magnific popup
+      $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+
+        gallery:{
+            enabled:true
+        }
+        // other options
+      });
+
+});
